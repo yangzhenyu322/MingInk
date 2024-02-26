d3009dddf219e8f4ef50d065f1d9df0222b3464a
yangzhenyu322 <2807975464@qq.com>
2024-02-26 22:15:59 +0800

    feat(article):新增mingink-module-article模块

---
+++ mingink-modules/mingink-modules-article/src/main/java/com/mingink/article/service/impl/ChapterService.java
@@ -0,0 +1,14 @@
+package com.mingink.article.service.impl;
+
+import com.mingink.article.service.IChapterService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: ZenSheep
+ * @Date: 2024/2/26 22:05
+ */
+@Slf4j
+@Service
+public class ChapterService implements IChapterService {
+}
---
+++ mingink-modules/mingink-modules-article/src/main/java/com/mingink/article/service/impl/TagService.java
@@ -0,0 +1,15 @@
+package com.mingink.article.service.impl;
+
+import com.mingink.article.service.ITagService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Service;
+
+/**
+ * @Author: ZenSheep
+ * @Date: 2024/2/26 22:03
+ */
+@Slf4j
+@Service
+public class TagService implements ITagService {
+
+}
