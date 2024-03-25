0d54e30486922f22ef6d2aebc03493cceb49ca16
yangzhenyu322 <2807975464@qq.com>
2024-03-25 16:09:52 +0800

    refactor(seata): 抽取seata部分为模块

--- mingink-modules/mingink-modules-order/src/main/java/com/mingink/order/service/strategy/pay/impl/AliPayStrategy.java
+++ mingink-modules/mingink-modules-order/src/main/java/com/mingink/order/service/strategy/pay/impl/AliPayStrategy.java
@@ -16,11 +16,11 @@ import com.mingink.order.domain.dto.PayRefundParams;
 import com.mingink.order.service.strategy.pay.IPayStrategy;
 import com.mingink.system.api.domain.dto.AliRefund;
 import io.seata.spring.annotation.GlobalTransactional;
+import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
-import javax.annotation.Resource;
 import java.math.BigDecimal;
 import java.util.HashMap;
 import java.util.Map;
