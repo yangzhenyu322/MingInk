3f79c25d3bd57743ba9347284f3c403150fd1a1d
ZenSheep <57444995+yangzhenyu322@users.noreply.github.com>
2024-04-23 20:05:39 +0800

    Revert "feax(pay): 增加微信支付工具类"

--- mingink-modules/mingink-modules-order/src/main/java/com/mingink/order/domain/dto/PayRefundParams.java
+++ mingink-modules/mingink-modules-order/src/main/java/com/mingink/order/domain/dto/PayRefundParams.java
@@ -2,19 +2,17 @@ package com.mingink.order.domain.dto;
 
 import lombok.Data;
 
-import java.io.Serializable;
-
 /**
  * 退款参数
  * @Author: ZenSheep
  * @Date: 2024/3/24 21:58
  */
-@SuppressWarnings("all")
 @Data
-public class PayRefundParams implements Serializable {
-    private static final long serialVersionUID = 1L;
+public class PayRefundParams {
     // 订单号
-    private String tradeNo;// 订单总金额
+    private String tradeNo;
+
+    // 订单总金额
     private String totalAmount;
 
     // 支付平台回调的订单流水号
@@ -22,56 +20,4 @@ public class PayRefundParams implements Serializable {
 
     // 支付方式
     private Integer payMode;
-
-    // 退款订单号（商家订单号）
-    private String outRefundNo;
-
-    // 退款资金来源
-    private String fundsAccount;
-
-    // 退款原因
-    private String reason;
-
-    private Amount amount;
-
-    private GoodDetail[] goodDetails;
-
-    /**
-     * 金额信息，必填
-     */
-    @Data
-    public class Amount {
-        private Integer refund;
-        private From[] froms;
-        private Integer total; // 单位为分
-        private String currency;
-
-        @Data
-        public class From {
-            private String account;
-            private Integer amount;
-        }
-    }
-
-    /**
-     * 退款商品，选填
-     */
-    @Data
-    private class GoodDetail {
-        private String merchantGoodsId;
-        // 商品编码，可空
-        private String wechatpayGoodsId;
-        // 商品名称，可空
-        private String goodsName;
-        // 商品单价，必填
-        private Integer unitPrice;
-        // 退款金额，必填
-        private Integer refundAmount;
-        // 退款数量，必填
-        private Integer refundQuantity;
-    }
 }
-
-
-
-
--- mingink-modules/mingink-modules-order/src/main/java/com/mingink/order/service/strategy/pay/impl/AliPayStrategy.java
+++ mingink-modules/mingink-modules-order/src/main/java/com/mingink/order/service/strategy/pay/impl/AliPayStrategy.java
@@ -9,10 +9,12 @@ import com.alipay.api.request.AlipayTradePagePayRequest;
 import com.alipay.api.request.AlipayTradeRefundRequest;
 import com.alipay.api.response.AlipayTradeRefundResponse;
 import com.mingink.order.config.AliPayConfig;
+import com.mingink.order.domain.dto.AliPay;
 import com.mingink.order.domain.dto.PayNotifyResult;
 import com.mingink.order.domain.dto.PayParams;
 import com.mingink.order.domain.dto.PayRefundParams;
 import com.mingink.order.service.strategy.pay.IPayStrategy;
+import com.mingink.system.api.domain.dto.AliRefund;
 import io.seata.spring.annotation.GlobalTransactional;
 import jakarta.annotation.Resource;
 import jakarta.servlet.http.HttpServletRequest;
@@ -150,6 +152,158 @@ public class AliPayStrategy implements IPayStrategy {
         }
 
         log.info("订单[{}]退款失败--{}", payRefundParams.getTradeNo(), payRefundParams);
+        return false;
+    }
+
+    // ************* 下面是原始方法 *************
+    @Override
+    public String getPayPage(AliPay aliPay) {
+        log.info("payInfo: {}", aliPay.toString());
+
+        // 1. 创建Client， 通过SDK提供的Client调用支付宝的API
+        AlipayClient alipayClient = new DefaultAlipayClient(AliPayConfig.GATEWAY_URL, aliPayConfig.getAppId(),
+                aliPayConfig.getAppPrivateKey(), AliPayConfig.FORMAT, AliPayConfig.CHARSET, aliPayConfig.getAlipayPublicKey(), AliPayConfig.SIGN_TYPE);
+
+        // 2. 创建 Request 并设置参数
+        AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();
+        request.setNotifyUrl(aliPayConfig.getNotifyUrl());
+        request.setReturnUrl(aliPayConfig.getReturnUrl());
+        JSONObject bizContent = new JSONObject();
+        bizContent.put("out_trade_no", aliPay.getTraceNo()); // 自己生成的订单号
+        bizContent.put("total_amount", aliPay.getTotalAmount()); // 订单的总金额
+        bizContent.put("subject", aliPay.getSubject()); // 支付的名称
+        // 销售产品码（必填），商家和支付宝签约的产品码。注：目前电脑支付场景下仅支持FAST_INSTANT_TRADE_PAY
+        bizContent.put("product_code", "FAST_INSTANT_TRADE_PAY");
+//        bizContent.put("product_code", aliPay.getProductCode()); // 固定配置
+        bizContent.put("qr_pay_mode", 2); // PC扫码的支付方式, 2-跳转模式，0、1、3、4-前置模式
+//        bizContent.put("qr_pay_mode", aliPay.getArPayMode()); // PC扫码的支付方式
+
+        request.setBizContent(bizContent.toString());
+
+        // 3. 执行请求
+        String form = "";
+        try {
+            form = alipayClient.pageExecute(request).getBody(); // 调用SDK生成表单
+        } catch (AlipayApiException e) {
+            e.printStackTrace();
+        }
+
+        return form;
+    }
+
+    @Override
+    public String getPayFront(AliPay aliPay) {
+        log.info("payInfo: {}", aliPay.toString());
+
+        // 1. 创建Client， 通过SDK提供的Client调用支付宝的API
+        AlipayClient alipayClient = new DefaultAlipayClient(AliPayConfig.GATEWAY_URL, aliPayConfig.getAppId(),
+                aliPayConfig.getAppPrivateKey(), AliPayConfig.FORMAT, AliPayConfig.CHARSET, aliPayConfig.getAlipayPublicKey(), AliPayConfig.SIGN_TYPE);
+
+        // 2. 创建 Request 并设置参数
+        AlipayTradePagePayRequest request = new AlipayTradePagePayRequest();
+        request.setNotifyUrl(aliPayConfig.getNotifyUrl());
+        request.setReturnUrl(aliPayConfig.getReturnUrl());
+        JSONObject bizContent = new JSONObject();
+        bizContent.put("out_trade_no", aliPay.getTraceNo()); // 自己生成的订单号
+        bizContent.put("total_amount", aliPay.getTotalAmount()); // 订单的总金额
+        bizContent.put("subject", aliPay.getSubject()); // 支付的名称
+        // 销售产品码（必填），商家和支付宝签约的产品码。注：目前电脑支付场景下仅支持FAST_INSTANT_TRADE_PAY
+        bizContent.put("product_code", "FAST_INSTANT_TRADE_PAY");
+        bizContent.put("qr_pay_mode", 4); // PC扫码的支付方式, 2-跳转模式，0、1、3、4-前置模式
+        bizContent.put("qrcode_width", aliPay.getQrcodeWidth());
+        request.setBizContent(bizContent.toString());
+
+        // 3. 执行请求
+        String form = "";
+        try {
+            form = alipayClient.pageExecute(request).getBody(); // 调用SDK生成表单
+        } catch (AlipayApiException e) {
+            e.printStackTrace();
+        }
+
+        return form;
+    }
+
+    /**
+     *
+     * @param request
+     * @return 消息报文，消息服务会根据响应报文判断商户系统是否已经成功处理消息。
+     * 如果HTTP同步响应报文返回 success 字符串，消息服务则认为消息已经处理成功，停止投递
+     * 如果返回 fail ，表示消息获取失败，支付宝会根据投递重试策略重新发送消息到应用网关地址
+     * @throws AlipayApiException
+     */
+    @Override
+    public String payNotify(HttpServletRequest request) throws AlipayApiException {
+        log.info("收到回调请求");
+        if (request.getParameter("trade_status").equals("TRADE_SUCCESS")) { // 交易成功
+            log.info("=========支付宝异步回调========");
+            Map<String, String> params = new HashMap<>();
+            Map<String, String[]> requestParams = request.getParameterMap();
+            for (String name : requestParams.keySet()) {
+                params.put(name, request.getParameter(name));
+            }
+
+            String tradeNo = params.get("out_trade_no");
+            String gmtPayment = params.get("gmt_payment");
+            String alipayTradeNo = params.get("trade_no"); // TODO 存储到数据库中
+
+            String sign = params.get("sign");
+            String content = AlipaySignature.getSignCheckContentV1(params);
+            boolean checkSignature = AlipaySignature.rsa256CheckContent(content, sign, aliPayConfig.getAlipayPublicKey(), AliPayConfig.CHARSET); // 验证签名
+            // 支付宝验签
+            if (checkSignature) {
+                // 验签通过
+                log.info("商户订单号: {}", params.get("out_trade_no"));
+                log.info("交易名称: {}", params.get("subject"));
+                log.info("支付宝交易凭证号: {}", params.get("trade_no"));
+                log.info("交易金额: {}", params.get("total_amount"));
+                log.info("实收金额: {}", params.get("receipt_amount"));
+                log.info("交易状态: {}", params.get("trade_status"));
+                log.info("买家在支付宝唯一id: {}", params.get("buyer_id"));
+                log.info("买家付款时间: {}", params.get("gmt_payment"));
+                log.info("买家付款金额: {}", params.get("buyer_pay_amount"));
+                log.info("支付成功");
+
+                return "success";
+            }
+        }
+
+        log.info("支付失败");
+        return "failure";
+    }
+
+    @Override
+    public Boolean refundPay(AliRefund aliRefund) throws AlipayApiException {
+        // 1. 创建client，通用SDK提供的Client，负责调用支付宝的API
+        AlipayClient alipayClient = new DefaultAlipayClient(AliPayConfig.GATEWAY_URL,
+                aliPayConfig.getAppId(), aliPayConfig.getAppPrivateKey(), AliPayConfig.FORMAT, AliPayConfig.CHARSET,
+                aliPayConfig.getAlipayPublicKey(), AliPayConfig.SIGN_TYPE);
+
+        // 2. 创建Request并设置 Reqeust 参数
+        AlipayTradeRefundRequest request = new AlipayTradeRefundRequest();
+        JSONObject bizContent = new JSONObject();
+        bizContent.put("trade_no", aliRefund.getAlipayTraceNo()); // 支付宝回调的订单流水号
+        bizContent.put("refund_amount", aliRefund.getTotalAmount()); // 订单的总金额
+        bizContent.put("out_request_no", aliRefund.getTraceNo()); // 我的订单号
+
+        // 返回参数选项，按需传入
+        //JSONArray queryOptions = new JSONArray();
+        //queryOptions.add("refund_detail_item_list");
+        //bizContent.put("query_options", queryOptions);
+
+        request.setBizContent(bizContent.toString());
+
+        // 3. 执行请求
+        AlipayTradeRefundResponse response = alipayClient.execute(request);
+        if (response.isSuccess()) { // 退款成功
+            log.info("订单[{}]退款成功--{}", aliRefund.getTraceNo(), aliRefund);
+
+            // 4. 更新数据库状态
+//            orderMapper.updatePayState(aliRefund.getTraceNo(), "已退款", now);
+            return true;
+        }
+
+        log.info("订单[{}]退款失败--{}", aliRefund.getTraceNo(), response.getBody());
         return false;
     }
 }
