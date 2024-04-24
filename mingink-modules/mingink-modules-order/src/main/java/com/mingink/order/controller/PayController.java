f3be2a41f4e102077a740899a6a1859daad9b751
hulx789 <627194985@qq.com>
2024-04-24 22:12:11 +0800

    feat(pay):微信支付工具

--- mingink-modules/mingink-modules-order/src/main/java/com/mingink/order/controller/PayController.java
+++ mingink-modules/mingink-modules-order/src/main/java/com/mingink/order/controller/PayController.java
@@ -24,34 +24,34 @@ import java.io.IOException;
 @RequestMapping("/pay")
 @Tag(name = "支付接口")
 public class PayController {
-    @Autowired
-    private IPayService payService;
-
-    @PostMapping("/page/payType/{payType}")
-    @Operation(hidden = true)
-//    @Operation(summary = "前往支付页面, payType可选值: aliPayStrategy-支付宝支付")
-    public R<String> payPage(@RequestBody AliPay aliPay, @PathVariable("payType") String payType) throws IOException {
-        return R.ok(payService.getPayPage(aliPay, payType));
-    }
-
-    @PostMapping("/front/payType/{payType}")
-    @Operation(hidden = true)
-//    @Operation(summary = "前置模式(存在iframe无法显示问题，需要https才能使用)")
-    public R<String> payFront(@RequestBody AliPay aliPay, @PathVariable("payType") String payType) {
-        return R.ok(payService.getPayFront(aliPay, payType));
-    }
-
-    @PostMapping("/notify/payType/{payType}")
-    @Operation(hidden = true)
-//    @Operation(summary = "回调接口, 用户支付成功后支付宝官方会调用的接口来改变订单状态，无需前端调用")
-    public String payNotify(@PathVariable("payType") String payType, HttpServletRequest request) throws AlipayApiException {
-        return payService.payNotify(request, payType);
-    }
-
-    @PutMapping("/refund/payType/{payType}")
-    @Operation(hidden = true)
-//    @Operation(summary = "退款接口")
-    public R<Boolean> refundPay(@RequestBody AliRefund aliRefund, @PathVariable("payType") String payType) throws AlipayApiException {
-        return R.ok(payService.refundPay(aliRefund, payType));
-    }
+//    @Autowired
+//    private IPayService payService;
+//
+//    @PostMapping("/page/payType/{payType}")
+//    @Operation(hidden = true)
+////    @Operation(summary = "前往支付页面, payType可选值: aliPayStrategy-支付宝支付")
+//    public R<String> payPage(@RequestBody AliPay aliPay, @PathVariable("payType") String payType) throws IOException {
+//        return R.ok(payService.getPayPage(aliPay, payType));
+//    }
+//
+//    @PostMapping("/front/payType/{payType}")
+//    @Operation(hidden = true)
+////    @Operation(summary = "前置模式(存在iframe无法显示问题，需要https才能使用)")
+//    public R<String> payFront(@RequestBody AliPay aliPay, @PathVariable("payType") String payType) {
+//        return R.ok(payService.getPayFront(aliPay, payType));
+//    }
+//
+//    @PostMapping("/notify/payType/{payType}")
+//    @Operation(hidden = true)
+////    @Operation(summary = "回调接口, 用户支付成功后支付宝官方会调用的接口来改变订单状态，无需前端调用")
+//    public String payNotify(@PathVariable("payType") String payType, HttpServletRequest request) throws AlipayApiException {
+//        return payService.payNotify(request, payType);
+//    }
+//
+//    @PutMapping("/refund/payType/{payType}")
+//    @Operation(hidden = true)
+////    @Operation(summary = "退款接口")
+//    public R<Boolean> refundPay(@RequestBody AliRefund aliRefund, @PathVariable("payType") String payType) throws AlipayApiException {
+//        return R.ok(payService.refundPay(aliRefund, payType));
+//    }
 }
